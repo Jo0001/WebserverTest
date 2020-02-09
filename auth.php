f3432a77e2f1261691812dbb8be1304954bae1b2
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2020-02-09 12:54:24 +0100

    Just testing some stuff

---
+++ auth.php
@@ -0,0 +1,58 @@
+<?php
+require "util.php";
+session_start();
+
+function logIn($email, $password, $location)
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=design_revision', 'dsnRev', '4_DiDsrev2019');
+    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $result = $statement->execute(array('email' => $email));
+    $user = $statement->fetch();
+
+    if ($user !== false && password_verify($password, $user['pswd'])) {
+        $_SESSION['user-status'] = $user['status'];
+        $_SESSION['user-log'] = "true";
+        $_SESSION['user-id'] = $user['pk_id'];
+        date_default_timezone_set('Europe/Berlin');
+        $_SESSION['user-time'] = date("Y-m-d H:i:s");
+        if (is_null($location)) {
+            $location = "../simulate/dashboard.php";
+        }
+        header("Location: $location");
+        die;
+    } else {
+        header("Location: ../login/?err=1");
+        die;
+    }
+
+}
+
+function isLoggedIn()
+{
+    if (isset($_SESSION['user-status']) && isset($_SESSION['user-id']) && isset($_SESSION['user-log']) && isset($_SESSION['user-time']) && $_SESSION['user-log'] == "true") {
+        date_default_timezone_set('Europe/Berlin');
+        $currentdate = date("Y-m-d H:i:s");
+        $usertime = $_SESSION['user-time'];
+        $diff = dateDifference($usertime, $currentdate);
+        if ($diff > 86400) {
+            logout();
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+function logOut()
+{
+    session_unset();
+    session_destroy();
+    header("Location: ../login/?success=logout");
+    die;
+}
+
+function getCurrentURL()
+{
+    //TODO Check if the url is a valid desing-revision url
+    return urlencode((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_STRING) . filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
+}
---
+++ util.php
@@ -0,0 +1,32 @@
+<?php
+//TODO Needs testing with more users
+function generateHash($pdo)
+{
+    do {
+        $hash = bin2hex(openssl_random_pseudo_bytes(64));
+        $statement = $pdo->prepare("SELECT * FROM users WHERE token = :token");
+        $result = $statement->execute(array('token' => $hash));
+        $token = $statement->fetch();
+    } while ($token !== false);
+    return $hash;
+}
+
+function parseHTML($file_path, $name, $link, $project_name, $project_version)
+{
+    $template = file_get_contents($file_path, false);
+    $template = str_replace("{name}", $name, $template);
+    $template = str_replace("{link}", $link, $template);
+    $template = str_replace("{project-name}", $project_name, $template);
+    $template = str_replace("{project-version}", $project_version, $template);
+    return $template;
+}
+
+/**
+ * @return time-difference in seconds
+ */
+function dateDifference($d1, $d2)
+{
+    $date1 = strtotime($d1);
+    $date2 = strtotime($d2);
+    return abs($date2 - $date1);
+}
